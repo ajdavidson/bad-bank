54605a2e9e00e5e0c9ea8a86bff3261b2f5fcbbc
AJDavidson <89638345+ajdavidson@users.noreply.github.com>
2022-01-11 15:59:13 -0700

    add show hide pwd input functionality

--- login.js
+++ login.js
@@ -6,6 +6,7 @@ function Login() {
   const [password, setPassword] = React.useState('');
   const ctx = React.useContext(UserContext);
 
+  const [passwordShown, setPasswordShown] = React.useState(false);
   // const res = ctx.users.filter(p => p.email == "richard.parker@oscorp.io")
   // console.log(res)
   //
@@ -31,25 +32,25 @@ function Login() {
     //
     const resEmail = ctx.users.filter(p => p.email == email)
     console.log(resEmail)
-    const resPwd = ctx.users.filter(p => p.password== password)
+    const resPwd = ctx.users.filter(p => p.password == password)
     console.log(resPwd)
 
 
-   // if (!validate(name, 'Enter a name')) return;
+    // if (!validate(name, 'Enter a name')) return;
     if (!validate(email, 'Enter an email')) return;
     if (!validate(password, 'Enter a password')) return;
-   //  if (password.length < 8) {
-   //    setStatus('password needs 8 characters min');
-   //    setTimeout(() => setStatus(''), 3000);
-   //    return;
-   //  }
-    if(resEmail.length === 0){
-     //alert("Value does not exists!")
+    //  if (password.length < 8) {
+    //    setStatus('password needs 8 characters min');
+    //    setTimeout(() => setStatus(''), 3000);
+    //    return;
+    //  }
+    if (resEmail.length === 0) {
+      //alert("Value does not exists!")
       setStatus("No Email Match");
       setTimeout(() => setStatus(''), 3000);
       return false;
     }
-    if(resPwd.length === 0){
+    if (resPwd.length === 0) {
       //alert("Value does not exists!")
       setStatus("No Password Match");
       setTimeout(() => setStatus(''), 3000);
@@ -61,16 +62,21 @@ function Login() {
   }
 
   function clearForm() {
-   // setName('');
+    // setName('');
     setEmail('');
     setPassword('');
     setShow(true);
   }
 
+  const togglePassword = () => {
+    // When the handler is invoked
+    // inverse the boolean state of passwordShown
+    setPasswordShown(!passwordShown);
+  };
   return (
     <React.Fragment>
       {/*<h5>Logged In {JSON.stringify(ctx.users)}</h5>*/}
-      <Card style={{ width: '400px' }}>
+      <Card style={{width: '400px'}}>
         <Card.Header><i className="fas fa-user"/> <b>Log In</b></Card.Header>
         <Card.Body>
           <Card.Title>
@@ -80,22 +86,37 @@ function Login() {
           {show ? (
             <>
               {/*  */}
-              Email address<br />
+              Email address<br/>
               {/* <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)} /><br /> */}
               <InputGroup className="mb-3">
                 <InputGroup.Text id="basic-addon1"><i class="fas fa-envelope"/></InputGroup.Text>
                 <FormControl
-                  type="email" className="form-control" id="email" placeholder="Enter your email" value={email} onChange={e => setEmail(e.currentTarget.value)}
+                  type="email" className="form-control" id="email" placeholder="Enter your email" value={email}
+                  onChange={e => setEmail(e.currentTarget.value)}
                 />
               </InputGroup>
               {/*  */}
-              Password<br />
+              Password<br/>
               {/* <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)} /><br /> */}
               <InputGroup className="mb-3">
                 <InputGroup.Text id="basic-addon1"><i class="fas fa-key"/></InputGroup.Text>
                 <FormControl
-                  type="password" id="password" placeholder="Enter a password" value={password} onChange={e => setPassword(e.currentTarget.value)}
+                  type={passwordShown ? "text" : "password"}
+                  id="password" placeholder="Enter a password" value={password}
+                  onChange={e => setPassword(e.currentTarget.value)}
                 />
+                <InputGroup.Text onClick={togglePassword} style={{width:'45px'}}>
+                  {passwordShown ? (
+
+                      <span><i className="fas fa-eye-slash"/></span>
+
+                  ):(
+
+                      <span><i className="fas fa-eye"/></span>
+
+                  )}
+                </InputGroup.Text>
+
               </InputGroup>
 
               {name !== '' || email !== '' || password !== '' ? (
@@ -118,13 +139,13 @@ function Login() {
       <h5><i className="fas fa-user-shield"/> Logged In as: {ctx.loggedIn[0].name}</h5>
       <br/>
       <h5><i className="fas fa-users-cog"/> Test Users</h5>
-      <Table striped hover style={{width:'400px'}}>
+      <Table striped hover style={{width: '500px'}}>
         <thead>
         <tr>
           <th>Level</th>
           <th>Email</th>
           <th>Password</th>
-          <th style={{textAlign:'center'}}><i className="fas fa-copy"/></th>
+          <th style={{textAlign: 'center'}}><i className="fas fa-copy"/></th>
         </tr>
         </thead>
         <tbody>
@@ -133,19 +154,22 @@ function Login() {
             <td>{t.level}</td>
             <td>{t.email}</td>
             <td>{t.password}</td>
-            <td style={{textAlign:'center'}}>
+            <td style={{textAlign: 'center'}}>
               <OverlayTrigger
                 placement='right'
                 overlay={
                   <Tooltip>
-                    Click to fill in form with this users info<br/>{t.email} <br/> {t.password}
+                    Click to fill in form <br/>with this users info<br/>{t.email} <br/> {t.password}
                   </Tooltip>
                 }
               >
-                <Button type="submit" variant="outline-*" onClick={() => {setEmail(t.email); setPassword(t.password)}}><i
+                <Button type="submit" variant="outline-*" onClick={() => {
+                  setEmail(t.email);
+                  setPassword(t.password)
+                }}><i
                   className="fas fa-copy"/></Button>
               </OverlayTrigger>
-              </td>
+            </td>
 
           </tr>
         ))}
