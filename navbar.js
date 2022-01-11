fbf5a6c8ec44eb99a9bb86737d008445f3e367f6
AJDavidson <89638345+ajdavidson@users.noreply.github.com>
2022-01-10 22:12:47 -0700

    clean up code

--- navbar.js
+++ navbar.js
@@ -1,18 +1,10 @@
 const {
   Container,
   Button,
-  Modal,
   Card,
-  Image,
-  ListGroup,
-  ListGroupItem,
   InputGroup,
   FormControl,
-  Row,
-  Col,
-  Spinner,
   Navbar,
-  NavDropdown,
   Nav,
   OverlayTrigger,
   Tooltip,
@@ -51,70 +43,6 @@ function NavBar() {
                 <Nav.Link className="nav-links" href={'#' + n.link}>{n.tab}</Nav.Link>
               </OverlayTrigger>
               ) )}
-              {/*/!* Home *!/*/}
-              {/*<OverlayTrigger*/}
-              {/*  placement='bottom'*/}
-              {/*  overlay={*/}
-              {/*    <Tooltip>*/}
-              {/*      Globalin Bank Online Application Welcome Page*/}
-              {/*    </Tooltip>*/}
-              {/*  }*/}
-              {/*>*/}
-              {/*  <Nav.Link className="nav-links" href="#/">Home</Nav.Link>*/}
-              {/*</OverlayTrigger>*/}
-              {/*/!* Create Account *!/*/}
-              {/*<OverlayTrigger*/}
-              {/*  placement='bottom'*/}
-              {/*  overlay={*/}
-              {/*    <Tooltip>*/}
-              {/*      Form to Create a new Account with Globalin Bank*/}
-              {/*    </Tooltip>*/}
-              {/*  }*/}
-              {/*>*/}
-              {/*  <Nav.Link href="#/CreateAccount/">Create Account</Nav.Link>*/}
-              {/*</OverlayTrigger>*/}
-              {/*/!* Deposit *!/*/}
-              {/*<OverlayTrigger*/}
-              {/*  placement='bottom'*/}
-              {/*  overlay={*/}
-              {/*    <Tooltip>*/}
-              {/*      Form to deposit money into your Account*/}
-              {/*    </Tooltip>*/}
-              {/*  }*/}
-              {/*>*/}
-              {/*  <Nav.Link href="#/deposit/">Deposit</Nav.Link>*/}
-              {/*</OverlayTrigger>*/}
-              {/*/!* Withdraw *!/*/}
-              {/*<OverlayTrigger*/}
-              {/*  placement='bottom'*/}
-              {/*  overlay={*/}
-              {/*    <Tooltip>*/}
-              {/*      Form to Withdraw money from your Account*/}
-              {/*    </Tooltip>*/}
-              {/*  }*/}
-              {/*>*/}
-              {/*  <Nav.Link href="#/withdraw/">Withdraw</Nav.Link>*/}
-              {/*</OverlayTrigger>*/}
-              {/*<OverlayTrigger*/}
-              {/*  placement='bottom'*/}
-              {/*  overlay={*/}
-              {/*    <Tooltip>*/}
-              {/*      View user data and history of transactions*/}
-              {/*    </Tooltip>*/}
-              {/*  }*/}
-              {/*>*/}
-              {/*  <Nav.Link href="#/alldata/">All Data</Nav.Link>*/}
-              {/*</OverlayTrigger>*/}
-              {/*<OverlayTrigger*/}
-              {/*  placement='bottom'*/}
-              {/*  overlay={*/}
-              {/*    <Tooltip>*/}
-              {/*      Log In to an Account*/}
-              {/*    </Tooltip>*/}
-              {/*  }*/}
-              {/*>*/}
-              {/*  <Nav.Link href="#/login/">Login</Nav.Link>*/}
-              {/*</OverlayTrigger>*/}
             </Nav>
           </Navbar.Collapse>
         </Container>
