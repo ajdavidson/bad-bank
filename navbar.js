971f36afe125b4ef91e5d9486dab90d85b211063
AJDavidson <89638345+ajdavidson@users.noreply.github.com>
2022-01-13 02:32:11 -0700

    update log in

--- navbar.js
+++ navbar.js
@@ -13,6 +13,12 @@ function NavBar() {
       {tip: 'View user data and history of transactions', link: 'alldata', tab: 'All Data'},
       {tip: 'Log In to an Account', link: 'login', tab: 'Login'},
     ]
+
+  function logOut() {
+    localStorage.clear();
+    window.location.reload(true);
+  }
+
   return (
     <>
       <Navbar bg="light" variant="light" expand="lg" style={{marginBottom: '0', paddingBottom: '0'}} sticky="top">
@@ -23,44 +29,56 @@ function NavBar() {
             <Nav className="me-auto" fill variant="tabs" defaultActiveKey="#/">
               {/*Build NAVBAR */}
               {navBuild.map((n, index) => (
-              <OverlayTrigger
-                key={index}
-                placement='bottom'
-                overlay={
-                  <Tooltip>
-                    {n.tip}
-                  </Tooltip>
-                }
-              >
-                <Nav.Link key={index} className="nav-links" href={'#' + n.link}>{n.tab}</Nav.Link>
-              </OverlayTrigger>
-              ) )}
+                <OverlayTrigger
+                  key={index}
+                  placement='bottom'
+                  overlay={
+                    <Tooltip>
+                      {n.tip}
+                    </Tooltip>
+                  }
+                >
+                  <Nav.Link key={index} className="nav-links" href={'#' + n.link}>{n.tab}</Nav.Link>
+                </OverlayTrigger>
+              ))}
             </Nav>
             {storedCTX !== null ? (
-                // <Dropdown style={{position: 'absolute', top: '15px', right: '10px'}}>
-                //   <Dropdown.Toggle variant="outline-*" id="dropdown-basic">
-                //     <h5><i className="fas fa-user-circle"/> {storedCTX.loggedIn[0].name}</h5>
-                //   </Dropdown.Toggle>
-                //
-                //   <Dropdown.Menu>
-                //     <Dropdown.Item href="#">Log Out</Dropdown.Item>
-                //     {/*<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>*/}
-                //     {/*<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>*/}
-                //   </Dropdown.Menu>
-                // </Dropdown>
+              // <Dropdown style={{position: 'absolute', top: '15px', right: '10px'}}>
+              //   <Dropdown.Toggle variant="outline-*" id="dropdown-basic">
+              //     <h5><i className="fas fa-user-circle"/> {storedCTX.loggedIn[0].name}</h5>
+              //   </Dropdown.Toggle>
+              //
+              //   <Dropdown.Menu>
+              //     <Dropdown.Item href="#">Log Out</Dropdown.Item>
+              //     {/*<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>*/}
+              //     {/*<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>*/}
+              //   </Dropdown.Menu>
+              // </Dropdown>
               <Dropdown as={ButtonGroup} style={{position: 'absolute', top: '10px', right: '10px'}}>
                 <Button variant="outline-*"><i className="fas fa-user-circle"/> {storedCTX.loggedIn[0].name}</Button>
 
-                <Dropdown.Toggle split variant="outline" id="dropdown-split-basic" />
+                <Dropdown.Toggle split variant="outline" id="dropdown-split-basic"/>
 
                 <Dropdown.Menu>
-                  <Dropdown.Item href="#">Log Out</Dropdown.Item>
+                  <Dropdown.Header>Clears All Saved Data</Dropdown.Header>
+                  <Dropdown.Divider />
+                  <Dropdown.Item onClick={logOut}>Log Out</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
               // <div style={{position: 'absolute', top: '15px', right: '10px'}}><h5><i className="fas fa-user-circle"/> {storedCTX.loggedIn[0].name}</h5></div>
 
-            ):(
-            <div style={{position: 'absolute', top: '15px', right: '10px'}}><h5><i className="fas fa-user-circle"/> Norman Osborn</h5></div>
+            ) : (
+              <Dropdown as={ButtonGroup} style={{position: 'absolute', top: '10px', right: '10px'}}>
+                <Button variant="outline-*"><i className="fas fa-user-circle"/> Norman Osborn</Button>
+
+                <Dropdown.Toggle split variant="outline" id="dropdown-split-basic"/>
+
+                <Dropdown.Menu>
+                  <Dropdown.Header>Default User</Dropdown.Header>
+                  <Dropdown.Divider />
+                  <Dropdown.Item disabled>Log Out</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
               )}
           </Navbar.Collapse>
         </Container>
